866c0fa2e73a15831706e553534fb21225604db1
Robert K. Paulsen <robert.paulsen@uib.no>
2026-02-16 12:42:51 +0100

    move base route for mdx files to root

--- source.config.ts
+++ source.config.ts
@@ -4,7 +4,7 @@ import { metaSchema, pageSchema } from 'fumadocs-core/source/schema';
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: 'content',
   docs: {
     schema: pageSchema,
     postprocess: {
