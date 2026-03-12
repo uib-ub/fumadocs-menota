f23438142dea0569f182930c16b3260117a45bbb
Robert K. Paulsen <robert.paulsen@uib.no>
2026-03-12 13:53:08 +0100

    introduce E-component

--- src/components/xml.tsx
+++ src/components/xml.tsx
@@ -4,29 +4,18 @@ import {
     transformerRemoveLineBreak 
 } from "@shikijs/transformers";
 
-const inlineTransformer = {
-    line(node: any) {
-        if (node.properties?.className?.includes('line')) {
-            node.children = node.children.flat();
-            delete node.properties?.className;
-        }
-    },
-    root(node: any) {
-        node.tagName = 'span';
-        delete node.properties;
-    }
-}
-
 export async function E({ children }: { children: string }) {
     const html = (await codeToHtml(`<${children}>`, {
         lang: 'xml', 
         theme: 'github-light',
         transformers: [
             transformerCompactLineOptions(),
-            transformerRemoveLineBreak(),
-            inlineTransformer
+            transformerRemoveLineBreak()
         ]
-    })).replace(/ class="line"/, ''); // Yikes, dirty hack!
+    })).replace(/<span class="line">/, '<span>'); // Yikes, dirty hack!
     console.log(html);
-    return <span className="inline-flex" dangerouslySetInnerHTML={{ __html: html }}/>
+    return <span 
+        suppressHydrationWarning 
+        className="xml-tag inline-flex" 
+        dangerouslySetInnerHTML={{ __html: html }}/>
 }
