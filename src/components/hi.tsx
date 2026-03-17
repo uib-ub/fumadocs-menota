dae9f2d6889a98c5ed462c71560f2bd541477cd0
Robert K. Paulsen <robert.paulsen@uib.no>
2026-03-17 16:03:02 +0100

    improve changelog

--- src/components/hi.tsx
+++ src/components/hi.tsx
@@ -4,8 +4,7 @@ export default function HI({ color, style, children }: {
     children: React.ReactNode }
 ) {
     return (
-        <span className={`
-            highlight
+        <span className={`highlight
             ${color ? `text-${color}-500` : ''}
             ${style == 'bold' ? 'font-bold' : ''}
         `}>
