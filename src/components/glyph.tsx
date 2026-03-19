bab735c63a80747e39f922c2247dd575126438c2
Robert K. Paulsen <robert.paulsen@uib.no>
2026-03-19 14:51:46 +0100

    active Runic font

--- src/components/glyph.tsx
+++ src/components/glyph.tsx
@@ -1,3 +1,3 @@
-export default function Glyph({ children }: { children: React.ReactNode }) {
+export default function Glyph({ children }: { children: string }) {
     return <span className="text-2xl">{children}</span>;
 }
