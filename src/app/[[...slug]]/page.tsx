1a37a53e77dc15f37ccb7d995db4465ed82af419
Robert K. Paulsen <robert.paulsen@uib.no>
2026-03-19 09:54:32 +0100

    remove unused component

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -53,17 +53,6 @@ export default async function Page(props: PageProps<'/[[...slug]]'>) {
             </div>
           );
       }})()}
-      {/*
-      <DocsTitle>{page.data.title}</DocsTitle>
-      <DocsDescription className="mb-0">{page.data.description}</DocsDescription>
-      <div className="flex flex-row gap-2 items-center border-b pb-6">
-        <LLMCopyButton markdownUrl={`${page.url}.mdx`} />
-        <ViewOptions
-          markdownUrl={`${page.url}.mdx`}
-          githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/content/docs/${page.path}`}
-        />
-      </div> 
-      */}
       <DocsBody>
         <MDX
           components={getMDXComponents({
