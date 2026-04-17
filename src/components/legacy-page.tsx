f07355ce913cced26375abf9c2212a556a7488b3
Robert K. Paulsen <robert.paulsen@uib.no>
2026-04-17 13:14:22 +0200

    ch2: basics

--- src/components/legacy-page.tsx
+++ src/components/legacy-page.tsx
@@ -5,18 +5,15 @@ import EmbeddedPage from "@/components/embedded-page";
 export default function LegacyPage({ slug }: { slug: string[] }) {
     const notFound = <div>Page not found.</div>;
     if (slug.length < 2) return notFound;
+    const version = slug[1].substring(1);
     const pageName = slug[2] || 'contents';
-    switch(slug[1]) {
-        case "v1-0": {
+    switch(version) {
+        case "1-0":
+        case "1-1":
             const source = pageName == 'literature' ? 
                 `/legacy/handbook/v1/HB1_${pageName}.html` :
-                `/legacy/handbook/v1/HB1-0_${pageName}.html`;
+                `/legacy/handbook/v1/HB${version}_${pageName}.html`;
             return <EmbeddedPage src={source} name={pageName}/>;
-        }
-        case "v1-1": {
-            const source = `/legacy/handbook/v1/HB1-1_${pageName}.html`;
-            return <EmbeddedPage src={source} name={pageName}/>;
-        }
     }
     return <p>{slug.join(" | ")}</p>;
 }
