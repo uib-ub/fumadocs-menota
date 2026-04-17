9acb9d15eb2a1e35350d2b14a6c15c89c60cf448
Robert K. Paulsen <robert.paulsen@uib.no>
2026-04-17 15:01:32 +0200

    appendices: charlist, dtd, editors, fonts

--- src/components/legacy-page.tsx
+++ src/components/legacy-page.tsx
@@ -5,11 +5,14 @@ import EmbeddedPage from "@/components/embedded-page";
 export default function LegacyPage({ slug }: { slug: string[] }) {
     const notFound = <div>Page not found.</div>;
     if (slug.length < 2) return notFound;
-    const version = slug[1].substring(1);
     const pageName = slug[2] || 'contents';
+    const version = pageName == 'dtd' && slug[1] == "v1-1" ? 
+        '1-1-1' :
+        slug[1].substring(1);
     switch(version) {
         case "1-0":
         case "1-1":
+        case "1-1-1":
             const source = pageName == 'literature' ? 
                 `/legacy/handbook/v1/HB1_${pageName}.html` :
                 `/legacy/handbook/v1/HB${version}_${pageName}.html`;
