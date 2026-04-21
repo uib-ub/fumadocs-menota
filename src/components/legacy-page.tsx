fe2201659ebefb4ca12c8f4174f774db5fbb0c16
Robert K. Paulsen <robert.paulsen@uib.no>
2026-04-21 14:21:33 +0200

    remaining appendices

--- src/components/legacy-page.tsx
+++ src/components/legacy-page.tsx
@@ -6,17 +6,17 @@ export default function LegacyPage({ slug }: { slug: string[] }) {
     const notFound = <div>Page not found.</div>;
     if (slug.length < 2) return notFound;
     const pageName = slug[2] || 'contents';
-    const version = pageName == 'dtd' && slug[1] == "v1-1" ? 
-        '1-1-1' :
-        slug[1].substring(1);
-    switch(version) {
-        case "1-0":
-        case "1-1":
-        case "1-1-1":
-            const source = pageName == 'literature' ? 
-                `/legacy/handbook/v1/HB1_${pageName}.html` :
-                `/legacy/handbook/v1/HB${version}_${pageName}.html`;
-            return <EmbeddedPage src={source} name={pageName}/>;
-    }
-    return <p>{slug.join(" | ")}</p>;
+    const version = (() => {
+        switch (pageName) {
+            case "dtd":
+                if (slug[1] == "v1-1") return '1-1-1';
+                break;
+            case "literature":
+                if (slug[1] == "v1-0") return '1';
+                break;
+        }
+        return slug[1].substring(1);
+    })();
+    const source = `/legacy/handbook/v1/HB${version}_${pageName}.html`;
+    return <EmbeddedPage src={source} name={pageName}/>;
 }
