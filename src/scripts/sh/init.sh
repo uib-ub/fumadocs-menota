bdb7b787340cd9d4d81b9ca48ef83ec82a35351a
Robert K. Paulsen <robert.paulsen@uib.no>
2026-05-19 13:30:13 +0200

    remove public/legacy

--- src/scripts/sh/init.sh
+++ src/scripts/sh/init.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+echo "Populating public/legacy/ ..."
+
 # Remove possible artefacts from previous build
-rm -rf public/legacy/handbook
+rm -rf public/legacy
 
 # Create directories for legacy data
-mkdir -p public/legacy/handbook/v1
+mkdir -p public/legacy/{handbook/v1,documents/{council,depo,editorial-board,statutes}}
 
 # Populate
+cp legacy/content-menota/dokumenter/DOK_raad*.html public/legacy/documents/council
+cp legacy/content-menota/dokumenter/DOK_depo*.html public/legacy/documents/depo
+cp legacy/content-menota/dokumenter/DOK_Referat*.html public/legacy/documents/editorial-board
+cp legacy/content-menota/dokumenter/DOK_vedtekter-utkast.html public/legacy/documents/statutes
 cp legacy/content-menota/handbok/handbok_1/*.html public/legacy/handbook/v1
