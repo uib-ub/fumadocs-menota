b91ae71ba09ea0a056ce469afbef54861106da20
Robert K. Paulsen <robert.paulsen@uib.no>
2026-08-14 09:14:11 +0200

    move styles

--- src/app/[lang]/(home)/layout.tsx
+++ src/app/[lang]/(home)/layout.tsx
@@ -2,13 +2,90 @@ import { baseOptions } from '@/lib/layout.shared';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import './home.css';
 
+const translations: Record<
+  "archive" | "hb" | "main_archive" | "news" | "org" | "other_archives",
+  {en: string, no: string}
+> = {
+  archive: {
+    en: "Text Archive",
+    no: "Tekstarkiv"
+  },
+  hb: {
+    en: "Handbook",
+    no: "Håndbok"
+  },
+  main_archive: {
+    en: "Main Archive",
+    no: "Hovedarkiv"
+  },
+  news: {
+    en: "News",
+    no: "Nyheter"
+  },
+  org: {
+    en: "Organisation",
+    no: "Organisasjon"
+  },
+  other_archives: {
+    en: "Other archives",
+    no: "Andre arkiv"
+  }
+}
+
 export default async function Layout({ params, children }: LayoutProps<'/[lang]'>) {
   const base = baseOptions();
-  const { lang } = await params;
+  const lang = (await params).lang as "en" | "no";
   return (  
     <HomeLayout
       className='min-h-screen flex'
       {...base}
+      links={[
+        {
+          text: translations.news[lang],
+          url: '/news'
+        },
+        {
+          type: 'menu',
+          text: translations.archive[lang],
+          items: [
+            {
+              text: translations.main_archive[lang],
+              url: 'https://clarino.uib.no/menota/catalogue/menota'
+            },
+            {
+              text: translations.other_archives[lang],
+              url: '/other/archives'
+            }
+          ]
+        },
+        {
+          type: 'menu',
+          text: translations.hb[lang],
+          items: [
+            {
+              text: `${translations.hb[lang]} 1.0`,
+              url: '/handbook/v1-0'
+            },
+            {
+              text: `${translations.hb[lang]} 1.1`,
+              url: '/handbook/v1-1'
+            },
+            {
+              text: `${translations.hb[lang]} 2`,
+              url: '/handbook/v2'
+            },
+            {
+              text: `${translations.hb[lang]} 3`,
+              url: '/handbook/v3'
+            }
+          ]
+        },
+        {
+          type: 'menu',
+          text: translations.org[lang],
+          items: []
+        }
+      ]}
     >
       <div>
         <div className='fixed inset-0 bg-[url("/images/nordr.jpeg")] bg-cover bg-right -z-10'/>
