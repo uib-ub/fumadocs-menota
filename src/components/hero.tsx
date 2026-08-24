3bc099bc9b1f9764e3b8e2ba1de7ed7c79e478c3
Robert K. Paulsen <robert.paulsen@uib.no>
2026-08-24 13:38:41 +0200

    adjust layout

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,8 +2,8 @@ import AutoImage from "./auto-image"
 
 export default function Hero({ children }: { children: React.ReactNode }) {
     return (
-        <div className="md:h-screen md:w-screen relative overflow-hidden" id="hero">
-            <div className="relative z-10 h-9/12 grid grid-cols-1 place-content-around">
+        <div className="md:h-screen md:w-screen md:relative md:overflow-hidden" id="hero">
+            <div className="md:relative z-10 h-9/12 grid grid-cols-1 place-content-around">
                 <AutoImage src='/images/Menota-banner-new.svg' className="
                     m-1 p-2 rounded-lg
                     sm:m-5 sm:p-3 sm:rounded-xl
@@ -11,9 +11,9 @@ export default function Hero({ children }: { children: React.ReactNode }) {
                     bg-white/70 backdrop-blur-xs
                 "/>
                 <div className="
-                    mt-5 m-1 mb-5 p-3 rounded-lg text-sm bg-black/30 
+                    m-1 mb-5 p-3 rounded-lg text-sm bg-black/30 
                     sm:m-5 sm:rounded-xl sm:text-base 
-                    md:mt-10 md:m-10 md:max-w-170 md:p-10 md:rounded-3xl md:text-lg/relaxed md:bg-black/0
+                    md:m-10 md:max-w-170 md:p-10 md:rounded-3xl md:text-lg/relaxed md:bg-black/0
                     [@media(max-height:750px)]:max-w-250
                     [@media(max-height:700px)]:-mt-5
                     backdrop-blur-xs
