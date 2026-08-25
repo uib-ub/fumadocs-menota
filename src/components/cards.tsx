f97cca53bea017341a63b85fb8d8b8f3381f71cb
Robert K. Paulsen <robert.paulsen@uib.no>
2026-08-25 12:56:51 +0200

    make cards single column on small screen

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,5 +1,5 @@
 export function Cards({ children }: { children: React.ReactNode }) {
-    return (<div className="mt-5 grid grid-cols-2 gap-3">{children}</div>);
+    return (<div className="mt-5 grid grid-cols-1 sm:grid-cols-2 gap-3">{children}</div>);
 }
 
 export function Card({ title, href, children }: { 
@@ -15,7 +15,7 @@ export function Card({ title, href, children }: {
             text-slate-700 dark:text-slate-300
             @max-lg:col-span-full hover:bg-green-200/20
         ">
-            <h3 className="mb-1 text-lg font-medium ">{title}</h3>
+            <h3 className="mb-1 text-lg font-medium text-start">{title}</h3>
             <div>{children}</div>
         </Frame>
     );
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero({ children }: { children: React.ReactNode }) {
         <div className="md:h-screen md:w-screen md:relative md:overflow-hidden" id="hero">
             <div className="md:relative z-10 h-9/12 grid grid-cols-1 place-content-around">
                 <AutoImage src='/images/Menota-banner-new.svg' className="
-                    m-1 p-2 rounded-lg
+                    my-5 m-1 p-2 rounded-lg
                     sm:m-5 sm:p-3 sm:rounded-xl
                     md:m-10 md:max-w-3/5 md:min-w-170 md:p-10 md:rounded-3xl 
                     bg-white/70 backdrop-blur-xs
